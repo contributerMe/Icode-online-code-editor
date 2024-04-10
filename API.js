63bb02c713399956279111fcb419c5c875037579
Nimesh <nmaslekar777@gmail.com>
2024-04-10 19:50:29 +0530

    some more chages

--- API.js
+++ API.js
@@ -20,31 +20,6 @@ app.post("/compile", function (req, res) {
     var input = req.body.input
     var lang = req.body.lang
     try {
-
-        if (lang == "Cpp") {
-            if (!input) {
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
-                compiler.compileCPP(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                });
-            }
-            else {
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
-                compiler.compileCPPWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                });
-            }
-        }
         if (lang == "Cpp") {
             if (!input) {
                 var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
---
+++ temp/nj1pm5p.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+
+    return 0;
+}
